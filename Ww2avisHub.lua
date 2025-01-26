85078bb3f1c1f8cd0a595442820f34607520db91
Ww1avi <kandonmezmomo@gmail.com>
2025-01-26 14:23:13 +0100

    Ww2avisHub.lua

--- Ww2avisHub.lua
+++ Ww2avisHub.lua
@@ -1,5 +1,6 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
+
 local Window = Fluent:CreateWindow({
     Title = "Ww2avi's Script" .. Fluent.Version,
     SubTitle = "by Ww2avi",
@@ -9,3 +10,28 @@ local Window = Fluent:CreateWindow({
     Theme = "Dark",
     MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
+
+
+Fluent:Notify({
+        Title = "Script Executed",
+        Content = "The Script Loaded Succesfully!",
+        SubContent = "SubContent", -- Optional
+        Duration = 8 -- Set to nil to make the notification not disappear
+})
+
+
+-- Fluent provides Lucide Icons, they are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "aperture" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+
+Tab:AddButton({
+    Title = "Auto Lift Weight",
+    Description = "This button Auto Lifts Weight",
+    Callback = function()
+        print("Auto Lift Weight Started!")
+    end
+})
+
