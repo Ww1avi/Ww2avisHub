71427be5f2d264b9d4022617b1dfc7cf55769fda
Ww1avi <kandonmezmomo@gmail.com>
2025-01-26 13:42:29 +0100

    Ww2avisHub.lua

---
+++ Ww2avisHub.lua
@@ -0,0 +1,17 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "Ww2avi's Script" .. Fluent.Version,
+    SubTitle = "by Ww2avi",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+
+-- Fluent provides Lucide Icons, they are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "🗻" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
